a3b12421bb6cd87ed3de0ffaae0c39f05e3ac093
g463yama <g520463m@mails.cc.ehime-u.ac.jp>
2024-06-26 10:58:33 +0900

    Update main.c
    FIxed

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <stdio.h>
+#include <math.h>
 
-int myRoot(double x) // x の平方根を返す関数。x >= 0 ではないときエラー表示を行い exit(1) とする。 
+double myRoot(double x) // x の平方根を返す関数。x >= 0 ではないときエラー表示を行い exit(1) とする。 
 {
   int y;
-  if( x <= 0 ){
-    printf("エラー：0以上の値ではありません in myRoot()\n");　      
+  if( !(x >= 0) ){//正の数値以外
+    printf("エラー：条件に合わない数値です in myRoot()\n");　      
     exit(1);
   }
   y = sqrt(x);
@@ -13,7 +15,7 @@ int myRoot(double x) // x の平方根を返す関数。x >= 0 ではないと�
 
 int main(void)
 {
-  int i;
+  int i = 0;//初期化
   double *x = (double *)malloc(sizeof(double) * 100);
 
   while( i <= 100 ){
